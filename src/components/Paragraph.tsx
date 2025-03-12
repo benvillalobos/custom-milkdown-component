8f43d745d4b1c89e2f08dae6fd13446f3d5485b4
Ben Villalobos <4691428+benvillalobos@users.noreply.github.com>
2025-03-12 11:03:40 -0700

    Update paragraph

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,5 +3,7 @@ import { useNodeViewContext } from '@prosemirror-adapter/react'
 
 export const Paragraph: React.FC = () => {
   const { contentRef, selected } = useNodeViewContext()
-  return <div style={{ outline: selected ? 'blue solid 1px' : 'none' }} role="presentation" ref={contentRef} />
+  return <div style={{ outline: selected ? 'blue solid 1px' : 'none' }} role="presentation" ref={contentRef}>
+    This is a div!
+    </div>
 }
